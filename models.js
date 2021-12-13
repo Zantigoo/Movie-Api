dc3bc81680d3ae4b51a895f303ad3f0745ff8243
Mathew Brookes <83518330+Zantigoo@users.noreply.github.com>
2021-12-13 17:17:33 -0500

    Changed back models

--- models.js
+++ models.js
@@ -1,12 +1,20 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
 
 let movieSchema = mongoose.Schema({
     Title: {type: String, required: true},
     Description: {type: String, required: true},
-    Genre: { type: mongoose.Schema.Types.ObjectId, ref: 'Genre' },
-    Director: {type: mongoose.Schema.Types.ObjectId, ref: 'Director' },
-    Imageurl: String,
+    Genre: {
+        Name: String,
+        Description: String, 
+    },
+    Director: {
+        Name: String,
+        Bio: String,
+        Birthyear: Number,
+        Deathyear: Number
+    },
+    Actors: [String],
+    ImagePath: String,
     Featured: Boolean
 });
 
@@ -15,16 +23,16 @@ let userSchema = mongoose.Schema({
     Password: {type: String, required: true},
     Email: {type: String, required: true},
     Birthday: Date,
-    Favmovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+    Favmovies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
 });
 
 userSchema.statics.hashPassword = (password) => {
     return bcrypt.hashSync(password, 10);
-  };
+};
   
   userSchema.methods.validatePassword = function(password) {
     return bcrypt.compareSync(password, this.Password);
-  };
+};
 
 let genreSchema = mongoose.Schema({
     Name: {type: String, required: true},
@@ -32,12 +40,11 @@ let genreSchema = mongoose.Schema({
 });
 
 let directorSchema = mongoose.Schema({
-    Name: {type: String, required: true},
+    Name: String,
     Bio: String,
     Birthyear: Number,
     Deathyear: Number
-});
-
+})
 
 let Movie = mongoose.model('Movie', movieSchema);
 let User = mongoose.model('User', userSchema);
